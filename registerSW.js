f0386806ba76503f0df824a5a8c9dc7354146add
lospoloes <lospoloes@users.noreply.github.com>
2026-03-30 09:37:57 +0800

    fix: set base path for GitHub Pages

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ai-news-pwa/sw.js', { scope: '/ai-news-pwa/' })})}
